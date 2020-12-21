69195e0452daa4a7a746144f26d5cfd73bf0a660
Jared Lustig <70167622+jared-lustig@users.noreply.github.com>
2020-12-20 17:36:44 -0800

    New Stuff?

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -12,21 +12,21 @@ int main()
 	TicTacToe Player1('X');
 	TicTacToe Player2('O');
 
+	sf::Window window;
 
 	//These are just checks to make sure the game functions work
 	Player1.DisplayBoard(board);
 	Player1.insertPos(0, 0, board);
 	
-
 	Player2.DisplayBoard(board);
 	Player2.insertPos(0, 1, board);
 	
-
 	Player1.DisplayBoard(board);
+	//inserting different positions
 	Player1.insertPos(0, 1, board);
 	Player1.insertPos(1, 0, board);
 	Player1.insertPos(2, 0, board);
-	win = Player1.Win(board);
+	win = Player1.Win(board); //Testing Win function
 	if (win == true)
 		std::cout << "Win" << std::endl;
 	else
